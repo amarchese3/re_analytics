4973937c05d84176083c75ed0cf180fba50f234f
Austin <amarchese3@gmail.com>
2019-11-19 03:02:44 +0000

    Initial ZIllow api integrated, folter structure, data_scraping for getting data, env is virtaul env (This should probably not be in git)

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ market.
 
 Lets Get this Bread :p
 
+
+After Editing Virtual Env.
+pip freeze---- Append the req.txt file with new req
+
+Starting a New Virtual Env:
+Virtual Env
+https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+On windows to activate:
+.\env\Scripts\activate
--- data_scraping/zillow_scraping.py
+++ data_scraping/zillow_scraping.py
@@ -2,4 +2,33 @@
 This File is used to scrape data from Zillow
 """
 
+import zillow
+#
+# with open("./bin/config/zillow_key.conf", 'r') as f:
+#     key = f.readline().replace("\n", "")
 
+key = "X1-ZWz17j2ro8zpqj_8e096"
+
+print(key)
+
+address = "3400 Pacific Ave., Marina Del Rey, CA"
+postal_code = "90292"
+
+
+api = zillow.ValuationApi()
+# data = api.GetSearchResults(key, address, postal_code)
+
+zpid="2100641621"
+detail_data = api.GetZEstimate(key, zpid)
+
+
+print(detail_data.get_dict())
+
+
+zpid="2100641621"
+detail_data = api.GetComps(key, zpid)
+
+print("detail_data", detail_data['comps'])
+
+for i in detail_data['comps']:
+    print("Value",i.get_dict())
