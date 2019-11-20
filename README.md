b4f808ac79292c1da4eeb873e9ae605a1eb4f666
Austin <amarchese3@gmail.com>
2019-11-20 02:46:16 +0000

    Zillow scraper to pull listing price and listing info

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ Virtual Env
 https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
 On windows to activate:
 .\env\Scripts\activate
+
+WEB SCRAPING LINKS
+https://elitedatascience.com/python-web-scraping-libraries
+ZILLOW KNOWS WE'RE A COMPUTER, SH*T
+https://stackoverflow.com/questions/46623658/whats-the-best-way-to-scrape-data-from-zillow
+ 
+
+https://webharvy.com/whblog/2019/07/26/how-to-scrape-property-details-from-zillow-real-estate-listings/
--- data_scraping/zillow_scraping.py
+++ data_scraping/zillow_scraping.py
@@ -1,34 +1,49 @@
 """
 This File is used to scrape data from Zillow
 """
-
+from bs4 import BeautifulSoup
 import zillow
+import requests
 #
 # with open("./bin/config/zillow_key.conf", 'r') as f:
 #     key = f.readline().replace("\n", "")
 
 key = "X1-ZWz17j2ro8zpqj_8e096"
 
-print(key)
+def get_listing_price(address, postal_code):
+    """
 
-address = "3400 Pacific Ave., Marina Del Rey, CA"
-postal_code = "90292"
+    :param address: Full Address of Listing
+    :param postal_code: Zip Code of Listing
+    :return: Listing Price of Listing
+    """
+    api = zillow.ValuationApi()
+    detail_data = api.GetSearchResults(key, address, postal_code)
+    address_dict = detail_data.get_dict()
+    print(address_dict)
+    address_link = address_dict['links']['home_details']
+    zillow_page = requests.get(address_link, headers=get_headers())
+    soup = BeautifulSoup(zillow_page.content, 'html.parser')
+    listing_price_html = soup.find_all('span', class_='ds-value')
+    # Getting listing price html, getting text, converting to readable python format 100,000 to 100000, making int
+    listing_price = int(listing_price_html[0].get_text()[1:].replace(',', ''))
+    return listing_price
 
+def get_headers():
+    """
+    :return: Usable header to mimic a browser
+    """
+    # Creating headers.
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.8',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    }
+    return headers
 
-api = zillow.ValuationApi()
-# data = api.GetSearchResults(key, address, postal_code)
-
-zpid="2100641621"
-detail_data = api.GetZEstimate(key, zpid)
-
-
-print(detail_data.get_dict())
-
-
-zpid="2100641621"
-detail_data = api.GetComps(key, zpid)
-
-print("detail_data", detail_data['comps'])
-
-for i in detail_data['comps']:
-    print("Value",i.get_dict())
+address = "99 Garden St # 1, Hoboken, NJ"
+postal_code = "07030"
+listing_price = get_listing_price(address, postal_code)
+print(listing_price)
